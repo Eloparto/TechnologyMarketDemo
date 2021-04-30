2c9d48428dd2923fa6dba4980c601291f7368066
Eloparto <80689369+Eloparto@users.noreply.github.com>
2021-04-30 20:34:13 +0300

    Update ProductManager.java

--- onlineTechnologyMarket/src/onlineTechnologyMarket/ProductManager.java
+++ onlineTechnologyMarket/src/onlineTechnologyMarket/ProductManager.java
@@ -18,7 +18,6 @@ public class ProductManager {
 	public void showProducts(Product[] product) {
 		
 		for(int i = 0,a=1; i<product.length; i++,a++) {
-			System.out.println("--------------------------------------------------------------------------------------------------------------");
 
 			System.out.println(a+"|"+"Category Id: "+product[i].categoryId+"|"+"Product Id: "+product[i].id+"|"+"Product Name: "+product[i].productName+"|"+"Unit Price: "+product[i].unitPrice+"|"+"Stock status: "+product[i].unitStock+"|");
 			
